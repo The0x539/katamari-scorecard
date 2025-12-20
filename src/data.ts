3ac4b7171a1bdb3b3b69e09d775a3bf9d96de869
The0x539 <the0x539@gmail.com>
2025-12-20 03:09:28 -0600

    it's taking shape

--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,111 @@
 export const missionNames = [
-  "",
-  "UI_ERT_006", // Make a Star 1
-  "UI_ERT_007", // Make a Star 2
-  "UI_ERT_011", // Make a Star 4 (yes these are out of order)
-  "UI_ERT_009", // Make a Star 3
-  "UI_ERT_012", // Make a Star 5
-  "UI_ERT_013", // Make a Star 6
-  "UI_ERT_014", // Make a Star 7
-  "UI_ERT_015", // Make a Star 8
-  "UI_ERT_016", // Make a Star 9
-  "UI_ERT_017", // Make the Moon
-  "UI_ERT_018", // Make Cancer
-  "UI_ERT_019", // Make Cygnus
-  "",
-  "UI_ERT_020", // Make Corona Borealis (out of order)
-  "UI_ERT_021", // Make Pisces
-  "UI_ERT_022", // Make Virgo
-  "UI_ERT_023", // Make Ursa Major
-  "UI_ERT_027", // Make Gemini
-  "UI_ERT_028", // Make Taurus
-  "",
-  "UI_ERT_029", // Make the North Star
-  "UI_ERT_030", // Eternal 1
-  "UI_ERT_031", // Eternal 2
-  "UI_ERT_032", // Eternal 3
+  0,
+  6, // Make a Star 1
+  7, // Make a Star 2
+  11, // Make a Star 4 (yes these are out of order)
+  9, // Make a Star 3
+  12, // Make a Star 5
+  13, // Make a Star 6
+  14, // Make a Star 7
+  15, // Make a Star 8
+  16, // Make a Star 9
+  17, // Make the Moon
+
+  18, // Make Cancer
+  19, // Make Cygnus
+  0,
+  20, // Make Corona Borealis (out of order)
+  21, // Make Pisces
+  22, // Make Virgo
+  23, // Make Ursa Major
+  27, // Make Gemini
+  28, // Make Taurus
+  0,
+  29, // Make the North Star
+  30, // Eternal 1
+  31, // Eternal 2
+  32, // Eternal 3
 ];
+
+export const constellationMaxima = [
+  null,
+  null, // Make a Star 1
+  null, // Make a Star 2
+  null, // Make a Star 4 (yes these are out of order)
+  null, // Make a Star 3
+  null, // Make a Star 5
+  null, // Make a Star 6
+  null, // Make a Star 7
+  null, // Make a Star 8
+  null, // Make a Star 9
+  null, // Make the Moon
+
+  133, // Make Cancer
+  70, // Make Cygnus
+  null,
+  107, // Make Corona Borealis (out of order)
+  174, // Make Pisces
+  199, // Make Virgo
+  null, // Make Ursa Major
+  108, // Make Gemini
+  null, // Make Taurus
+];
+
+export const symbols = [
+  null,
+  null, // 1
+  null, // 2
+  null, // 3
+  null, // 4
+  null, // 5
+  null, // 6
+  null, // 7
+  null, // 8
+  null, // 9
+  "🌕", // Make the Moon
+
+  "♋", // Make Cancer
+  "🦢", // Make Cygnus
+  "0",
+  "👑", // Make Corona Borealis (out of order)
+  "♓", // Make Pisces
+  "♍", // Make Virgo
+  "🐻", // Make Ursa Major
+  "♊", // Make Gemini
+  "♉", // Make Taurus
+  "0",
+  "🧭", // Make the North Star
+];
+
+export class CowbearData {
+  constructor(
+    readonly monoID: string,
+    readonly resultName: number,
+    readonly resultSize: number,
+  ) {}
+}
+
+export const cowbearData = new Map([
+  [856, new CowbearData("MILKPACK01_C", 10, 110)],
+  [486, new CowbearData("COW01_D", 11, 111)],
+  [1358, new CowbearData("COWKANBAN_D", 12, 111)],
+  [958, new CowbearData("PILON10_D", 13, 112)],
+  [487, new CowbearData("COW02_E", 14, 112)],
+  [488, new CowbearData("COW03_E", 15, 112)],
+  [646, new CowbearData("ZIHANKI03_E", 16, 112)],
+  [731, new CowbearData("MANANIMAL03_D", 17, 112)],
+  [882, new CowbearData("PARASOL03_E", 18, 112)],
+  [489, new CowbearData("COW04_E", 19, 113)],
+  [490, new CowbearData("COW05_F", 20, 114)],
+
+  [1242, new CowbearData("TEDDYBEAR01_C", 1, 110)],
+  [468, new CowbearData("BEAR01_B", 21, 110)],
+  [469, new CowbearData("BEAR02_C", 3, 111)],
+  [470, new CowbearData("BEAR03_D", 7, 112)],
+  [471, new CowbearData("BEAR04_E", 8, 113)],
+  [472, new CowbearData("BEAR05_F", 9, 114)],
+  [730, new CowbearData("MANANIMAL02_D", 6, 112)],
+  [1002, new CowbearData("STATUE09_E", 5, 111)],
+  [877, new CowbearData("WOODYBEAR_C", 2, 110)],
+  [1115, new CowbearData("MOUNTSIGN02_D", 4, 111)],
+]);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import { render } from "preact";
 import { computed, effect, signal } from "@preact/signals";
-import type { JSX } from "preact";
+import type { ComponentChildren, JSX, TargetedInputEvent } from "preact";
 import { Mission, SaveFile } from "./save-file.ts";
 
 import Strings from "./strings.json" with { type: "json" };
-import { missionNames } from "./data.ts";
+import { constellationMaxima, cowbearData, missionNames } from "./data.ts";
+
+function swap<T>(arr: T[], i: number, j: number): void {
+  [arr[i], arr[j]] = [arr[j], arr[i]];
+}
+
+swap(missionNames, 3, 4); // bruh
 
 const STRINGS = Strings as Record<string, string[]>;
+
+function localize(category: string, id: number): string | null {
+  if (id == null) return null;
+  const key = category + "_" + id.toString().padStart(3, "0");
+  return STRINGS[key]?.[1];
+}
 
 const theFile = signal<File | null>(null);
 
@@ -17,10 +29,17 @@ effect(() => {
 
 const saveFile = computed(() => {
   if (buffer.value.byteLength === 0) return null;
-  return new SaveFile(buffer.value);
+  const save = new SaveFile(buffer.value);
+  swap(save.missions, 3, 4);
+  return save;
 });
 
-function MissionEntry(i: number, mission: Mission): JSX.Element {
+function MissionEntry(i: number, mission: Mission): JSX.Element | null {
+  let name = localize("UI_ERT", missionNames[i]);
+  if (!name) return null;
+  // it's MY tool and I get to make the rules
+  name = name.replace("the North Star", "Polaris");
+
   let size = (mission.clearSize % 10) + "mm";
   if (mission.clearSize > 10) {
     const cm = Math.floor(mission.clearSize / 10) % 100;
@@ -31,23 +50,101 @@ function MissionEntry(i: number, mission: Mission): JSX.Element {
     size = m + "m " + size;
   }
 
-  const name = STRINGS[missionNames[i]]?.[1];
-  if (name) {
-    return <li>{name}: {size}</li>;
-  } else {
-    return <li>[Mission #{i}]: {size}</li>;
+  const seconds = Math.round(mission.clearTime / 30);
+  // const milliseconds = Math.round(mission.clearTime * 1000 / 30) % 1000;
+  const time = Temporal.Duration.from({ seconds })
+    .round({ largestUnit: "minutes", smallestUnit: "seconds" })
+    .toLocaleString();
+
+  const pairs: Record<string, ComponentChildren> = {
+    "Size": size,
+    "Time": time,
+    "Present": mission.swPresent ? "🎁" : "🔎",
+    "Objects": mission.clearCatchCount,
+  };
+
+  if (mission.catchRanking[0]) {
+    pairs["Ranking"] = localize("OT_STR", mission.catchRanking[0]);
   }
+
+  if (mission.nameA) {
+    const data = cowbearData.get(mission.nameA);
+    if (data) {
+      const name = localize("OT_CON", data?.resultName);
+      const size = localize("UI_SYS", data?.resultSize);
+      pairs["Constellation object"] = `${name} (${size})`;
+    }
+  }
+
+  if (mission.catchCountB > 1) {
+    let count = mission.catchCountB.toString();
+
+    const max = constellationMaxima[i];
+    if (max) {
+      const pct = Math.floor(+count / max * 100);
+      count = `${count} / ${max} (${pct}%)`;
+    }
+
+    pairs["Constellation objects"] = count;
+  }
+
+  if (mission.catchRankCategory.some((n) => n !== 0)) {
+    // TODO: take catchRankName into account
+    // (represents tied placement, shows up ingame as e.g. 1st, 2nd, 2nd)
+    pairs["Categories"] = (
+      <ol>
+        {mission.catchRankCategory.map((n) => (
+          <li key={n}>{localize("OT_CTG", n)}</li>
+        ))}
+      </ol>
+    );
+  }
+
+  return (
+    <li>
+      <h2>{name}</h2>
+      <dl>
+        {Object.entries(pairs).map(([k, v]) => (
+          <>
+            <dt>{k}</dt>
+            <dd>{v}</dd>
+          </>
+        ))}
+      </dl>
+    </li>
+  );
 }
 
 function Scorecard(): JSX.Element | null {
   const file = saveFile.value;
-  if (!file) return null;
-  return (
+
+  const link =
+    "https://www.pcgamingwiki.com/wiki/Katamari_Damacy_Reroll#Save_game_data_location";
+
+  const body = file && (
     <>
-      <ol>
+      <h1>Missions</h1>
+      <ol class="missions">
         {file.missions.map((m, i) => MissionEntry(i, m))}
       </ol>
-      <pre>{JSON.stringify(saveFile.value, null, "\t")}</pre>
+
+      <button type="button" onClick={() => console.dir(file)}>
+        Dump full decoded save file to console
+      </button>
+    </>
+  );
+
+  const updateFile = (e: TargetedInputEvent<HTMLInputElement>) => {
+    theFile.value = e.currentTarget.files![0];
+  };
+
+  return (
+    <>
+      <input type="file" onInput={updateFile} />
+      <p>
+        or drag+drop <a target="_blank" href={link}>your save file</a>
+      </p>
+      {body}
     </>
   );
 }
